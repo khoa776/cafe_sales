2caefd7da9e5bfac53107880768b14485eeb26e6
khoa <khoa776lt@gmail.com>
2026-01-24 11:27:15 +0700

    feat: explore raw data and identify data quality issues

--- src/explore.py
+++ src/explore.py
@@ -1,16 +1,19 @@
-# This is a sample Python script.
+import pandas as pd
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+df = pd.read_csv("../data/raw/dirty_cafe_sales.csv")
+print("Shape of df: ", df.shape)
+print("\nInfo of df: ")
+print(df.info())
 
+print("\nMissing values: ")
+print(df.isnull().sum())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("\nDuplicate: ")
+print(df.duplicated().sum())
 
+print("\nItem: ")
+print(df['Item'].value_counts(normalize=True))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("\nFirst 5 items: ")
+print(df.head(5))
